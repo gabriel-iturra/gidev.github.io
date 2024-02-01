ca1cfc6a83b089fcf1056ef3c40eee45113d7314
gabriel-iturra <63883778+gabriel-iturra@users.noreply.github.com>
2024-02-01 12:18:28 +0200

    Reorder 2

---
+++ CuidadosVirtuales/js.js
@@ -0,0 +1,35 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+window.addEventListener("scroll", reveal);
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector('.navbar').classList.remove('hidden');
+    } else {
+        document.querySelector('.navbar').classList.add('hidden');
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+
+// Función con tiempo de variable
+function hideNavbar(time) {
+  setTimeout(function() {
+      document.querySelector('.navbar').classList.add('hidden');
+  }, time);
+}
